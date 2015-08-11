8863363289b0d5412674dd47cbc6aa6b8cd71c57
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2015-08-11 17:01:16 -0430

    Removin unnecesary files.

--- twemoji/generate.sh
+++ twemoji/generate.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-cat symbols.txt | while read SYM; do
-	FSYM=""
-	for SSYM in ${SYM}; do
-		FSYM=${FSYM}$(perl -C -e "print chr 0x${SSYM}")
-	done
+for i in $(cat symbols.txt); do
 
-	convert -size 25x25 \
-		xc:none \
-		-fill black \
-		-font Android-Emoji \
-		-gravity center \
-		-pointsize 22 \
-		-annotate +3+0 "${FSYM}" \
-		PNG:"${SYM,,}.png"
+    wget https://twemoji.maxcdn.com/svg/$i.svg
+
 done
