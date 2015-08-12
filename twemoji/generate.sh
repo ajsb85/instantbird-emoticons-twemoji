31a6b9c92a8429515114b3c11944648a6be87291
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2015-08-11 21:27:37 -0430

    Generating pngs from source.

--- twemoji/generate.sh
+++ twemoji/generate.sh
@@ -6,5 +6,6 @@ for FILE in $(ls -1 svg/*); do
     SVG="$(echo ${FILE} | awk -F/ '{print $2}')"
     PNG="$(echo ${SVG} | awk -F. '{print $1}').png"
 
-    convert -background None "${DIR}/${SVG}" "${PNG}"
+    echo "${SVG} > ${PNG}"
+    convert -size 25x25 -background None "${DIR}/${SVG}" "${PNG}"
 done
